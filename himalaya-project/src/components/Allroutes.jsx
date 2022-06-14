7f6c9de213063a1ffeecdeef4c533a866ea84484
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-14 17:06:20 +0530

    made

---
+++ himalaya-project/src/components/Allroutes.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "../pages/Home"
+
+
+
+
+
+export const Allroutes=(()=>{
+    return <Routes>
+        <Route path="/" element={<Home/>} />
+    </Routes>
+})
