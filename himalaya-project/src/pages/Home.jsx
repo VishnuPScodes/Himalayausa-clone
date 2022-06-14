79fc148485ef7ddd54651d4d78213a6d5aec0094
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-14 20:16:53 +0530

    2

--- himalaya-project/src/pages/Home.jsx
+++ himalaya-project/src/pages/Home.jsx
@@ -16,6 +16,23 @@ export const Home=(()=>{
         </div>
         <div id='shop-category' >
             <div id='text-1' >Shop By Category</div>
+            <div id='box-outer' >
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0399/1728/9633/files/HUSA-Ashwagandha-90ct-Carton_Bottle-square_1024x.png?v=1607451904" alt="" />
+                    <div id='bottom-text' >Supplements</div>
+                    <div id='bottom-text-2'>See All Supplemets</div>
+                </div>
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0399/1728/9633/files/WCC-SIMPLY-MINT-RENDER-WITH-BRUSH-High-Res_1024x.png?v=1607451904" alt="" />
+                    <div id='bottom-text' >Oral Care</div>
+                    <div id='bottom-text-2'>See All Oral Care</div>
+                </div>
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0399/1728/9633/files/Face_Wash-Balancing-Box-Bottle-square_1024x.png?v=1607451904" alt="" />
+                    <div id='bottom-text' >Personal Care</div>
+                    <div id='bottom-text-2'>See All Personal Care</div>
+                </div>
+            </div>
         </div>
     </div>
 })
