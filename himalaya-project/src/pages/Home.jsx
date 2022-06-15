86b9efc2b81507a016305eff8b2cb573142c98cb
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-15 11:23:20 +0530

    made changes

--- himalaya-project/src/pages/Home.jsx
+++ himalaya-project/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import '../components/styles/home.css'
-
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../components/styles/carousel.css";
+import { SliderData } from "./SliderData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -34,5 +41,116 @@ export const Home=(()=>{
                 </div>
             </div>
         </div>
+       <Carousel/>
+       <div id='bottom-div-2'>
+        <div id='inner-div-2' >
+            <img src="https://cdn.shopify.com/s/files/1/0399/1728/9633/files/StressCareA-Main-647x1024.png?v=1591359475" alt="" />
+            <div id='text-div-main'>
+                <span id='inner-text-1'>Unlock the Wisdom of Herbs</span>
+                <div  id='inner-text-2'>with Herbal Supplements, Toothpaste & More!</div>
+                <div id='para-div-1' style={{marginTop:"15px"}}>Since 1930, Himalaya has been passionate about the healing wisdom of herbs. We offer a wide range of clinically-studied herbal supplements, toothpaste and personal care products that unlock the powerful healing benefits of herbs.</div>
+            </div>
+        </div>
+       </div>
+
+
+
+
+
     </div>
 })
+
+const PreviousBtn = (props) => {
+    console.log(props);
+    const { className, onClick } = props;
+    return (
+      <div className={className} onClick={onClick}>
+        <FontAwesomeIcon
+          icon={faArrowLeft}
+          style={{ color: "black", fontSize: "30px" }}
+        />
+      </div>
+    );
+  };
+  const NextBtn = (props) => {
+    const { className, onClick } = props;
+    return (
+      <div className={className} onClick={onClick}>
+        <FontAwesomeIcon
+          icon={faArrowRight}
+          style={{ color: "black", fontSize: "30px" }}
+        />
+      </div>
+    );
+  };
+  
+  const carouselProperties = {
+    prevArrow: <PreviousBtn />,
+    nextArrow: <NextBtn />,
+    slidesToShow: 4,
+    // infinite={false}
+    // slidesToScroll={3}
+    centerMode: true,
+    centerPadding: "7px",
+    responsive: [
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 3,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  
+  const Carousel = () => {
+    return (
+      <div style={{ margin: "30px" }} className="carousel">
+        <h1 className="cHead" id='heading-text'>Best Sellers</h1>
+     
+        <Slider {...carouselProperties}>
+          {SliderData.map((item) => (
+            <Card item={item} />
+            
+          ))}
+      
+        </Slider>
+       
+      </div>
+    );
+  };
+  
+  const Card = ({ item }) => {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img
+          className="multi__image"
+          src={item}
+          alt=""
+          style={{
+            width: "100%",
+            height: "400px",
+            objectFit: "contain",
+            marginBottom: "10px",
+          }}
+        />
+      </div>
+    );
+  };
+  
+  export default Carousel;
---
+++ himalaya-project/src/pages/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Whitening-Mint---Box-Tube_800x.png?v=1597743896",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Himalaya_LiverCare_42ct_-_Carton_Bottle_800x.png?v=1654789186",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/ImmunoCare-120---Box-Bottle_800x.png?v=1595814320",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Neem-and-Pom---Box-Tube_800x.png?v=1597744049",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Ashwagandha_BoxBottle_30_800x.jpg?v=1591028054",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-23_800x.png?v=1590836890",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Bitter-Melon-30---Box-Bottle_800x.png?v=1595862276",
+  
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Gymnema_Box_Bottle_30s_800x.png?v=1591028093",
+    "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Ashwagandha_BoxBottle_30_800x.jpg?v=1591028054"
+  ];
