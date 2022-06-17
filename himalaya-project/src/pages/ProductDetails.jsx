0338ad343fdea6575d1fa4c829b8ea313346804e
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-17 20:34:22 +0530

    dd

--- himalaya-project/src/pages/ProductDetails.jsx
+++ himalaya-project/src/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 
+import { Button } from '@chakra-ui/react'
 import '../components/styles/products.css'
 
 
@@ -9,7 +10,20 @@ export const ProductDetails=(()=>{
           <div id='left-pro'>
             <div id='img-pro'></div>
           </div>
-          <div id='right-pro'></div>
+          <div id='right-pro'>
+            <div id='data-pro'>
+                <div id='text-pro' >  Bamboo & Sea Salt Whitening Antiplaque</div>
+                <div id='price-pro'>$3.99</div>
+                <div id='btns-pro'>
+                    <div id='plus-btns'>
+                        <div id='btn-1-min'>-</div>
+                        <div id='display-plus'></div>
+                        <div id='btn-1-plus'>+</div>
+                    </div>
+                    {/* <Button>-</Button> <Button>+</Button> */}
+                </div>
+            </div>
+          </div>
         </div>
     </div>
 })
