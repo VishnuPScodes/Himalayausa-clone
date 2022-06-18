84fa6ef1eae7a4db3a0181b9ed09eb87eb795e07
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-18 12:35:47 +0530

    peronal care complete

--- himalaya-project/src/pages/PersonalCare.jsx
+++ himalaya-project/src/pages/PersonalCare.jsx
@@ -7,15 +7,24 @@ import { useDispatch ,useSelector} from 'react-redux'
 import { productLoadingFalse, productLoadingTrue } from '../redux/action'
 import { Spinner } from '@chakra-ui/react'
 import { useNavigate } from 'react-router'
-
+import { RadioGroup,Radio } from '@chakra-ui/react'
 
 export const PersonalCare=(()=>{
+    const [change,setChange]=useState(false)
     const navigate=useNavigate()
     const loading=useSelector(state=>state.productLoading)
     const[items,setItems]=useState(12)
     const handleNext=(()=>{
         productLoadingTrue()
         setItems(items+6)
+    })
+    const handleCheck=((v)=>{
+        if(v==''){
+            setChange(!change)
+        }   
+        axios.get(`http://localhost:8080/personal?q=${v}`).then((res)=>{
+            setData(res.data)
+        })
     })
     const [data,setData]=useState([])
     const dispatch=useDispatch()
@@ -24,14 +33,26 @@ export const PersonalCare=(()=>{
          setData(res.data)
          dispatch(productLoadingFalse())
       })
-    },[items])
+    },[items,change])
     return <div
-    > <h1>Oral Care</h1>
+    > 
         <div style={{display:"flex"}}>
        
-            <div id="side-1">Filter</div>
+            <div id="side-1">
+            <div id='health-interest'>Product Type</div>
+            <div id='all-property'> 
+                <RadioGroup id='single-property' onChange={handleCheck} >
+                    <Radio value={''} >All</Radio> <br />
+                    <Radio value={'face'} >Face Care</Radio> <br />
+                    <Radio value={'bar'} >Cleansing Bars</Radio> <br />
+                    <Radio value={'balm'} >Balms</Radio> <br />
+                    
+                </RadioGroup>
+                </div>
+            </div>
             <div id="contend">
-                <div id='sort' >ff</div>
+            <div id='sort' >Personal Care</div>
+            <div id='sort-2'></div>    
                 <div id='data-map'>
                     {data.map((e)=>{
                         return (
@@ -41,11 +62,13 @@ export const PersonalCare=(()=>{
                        
                             <img src={e.url} alt="image" />
                         
-                        <div style={{textAlign:"left"}}>Bamboo & Sea Salt Whitening Antiplaque Toothpaste</div>
-                        <div style={{display:"flex",height:"45px",border:"1px solid blue",marginTop:"20px"}}>
+                        <div style={{textAlign:"left"}}>{e.name}</div>
+                        <div style={{display:"flex",height:"45px",marginTop:"20px"}}>
                             <div style={{width:"50px",backgroundColor:"#a3b49b"}} >
                              <BsHandbag style={{margin:"auto",marginTop:"5px",height:"30px"}} />
                             </div>
+                            <div id='price-3'>${e.price}</div>
+                            <div id='hover-cart' style={{backgroundColor:"#a3b49b",width:"110px"}}>ADD TO CART</div>
                         </div>
                     </div>
 
